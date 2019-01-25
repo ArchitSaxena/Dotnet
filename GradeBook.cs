079bb7759252e19d5a59d2bca36ffafb031ec89f
Archit Saxena <Archit.Saxena@mrcooper.com>
2019-01-25 12:51:15 +0530

    initial commit

---
+++ GradeBook.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grade
+{
+    class GradeBook
+    {
+        List<Double> grades;
+        public GradeBook()
+        {
+             grades = new List<Double>();
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats= new GradeStatistics();
+            double sum = 0;
+            int c = 0;
+            
+            foreach(double grade in grades)
+            {
+                sum = sum + grade;
+                c++;
+                if (grade > stats.MaxGrade)
+                    stats.MaxGrade = grade;
+                stats.MinGrade = Math.Min(grade, stats.MinGrade);
+            }
+            stats.AvgGrade = sum / grades.Count;
+            return stats;
+        }
+        public void addGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+    }
+}
---
+++ GradeStatistics.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grade
+{
+    class GradeStatistics
+    {
+        public double AvgGrade;
+        public double MaxGrade;
+        public double MinGrade;
+        public GradeStatistics()
+        {
+            MaxGrade = double.MinValue;
+            MinGrade = double.MaxValue;
+        }
+    }
+}
